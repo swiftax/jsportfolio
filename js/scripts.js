a4b5ca1295a939d0e6e579961e4fa50469eaeff8
Anthony Tropeano <tony@swiftax.com>
2015-06-22 08:37:15 -0400

    revision for conditionals and arrays

--- js/scripts.js
+++ js/scripts.js
@@ -44,27 +44,21 @@ $("#submit-button").on("click" , function() {
 		$("#charCount").html(charCount);
 
 		if (charCount > 150) {
-			$("#charCount").css("color", "red");
-			$("#charCount").text("You exceeded the maximum character limit");
+			$("#charCount").css("color", "red").text("You exceeded the maximum character limit");
 		} else {
 			$("#charCount").css("color", "white");
 		};
-	})
-
-	$("#email").on("keyup", function() {
-		console.log("keyup with email!");
-		var charCount = $("#email").val().length
-		console.log(charCount);
-		$("#charCount").html(charCount);
-		if (charCount > 30) {
-			$("#charCount").css("color", "red");
-			$("#charCount").text("Invalid Email Address");
-		} else {
-			$("#charCount").css("color", "pink");
-		};
 	});
-
 });
+	/* FAQ Section */
+
+	var rows = $(".my-row");
+
+	for (var i=0; i< rows.length; ++i) {
+		if (i%2===0) {
+	$(rows[i]).css("background-color","pink");
+	};
+};
 	/* Google Maps */
 
       function initialize() {
@@ -87,22 +81,7 @@ $("#submit-button").on("click" , function() {
       }
       google.maps.event.addDomListener(window, 'load', initialize);
 
-	/* FAQ Section */
 
-	var rows = $(".my-row");
-
-	for (var i=0; i< rows.length; ++i) {
-		if (i%2===0) {
-	$(rows[i]).css("background-color","#376a74");
-	$(rows[i]).css("color", "white");
-
-	} else {
-	$(rows[0]).css("background-color","#000");
-	$(rows[0]).css("color", "fff");
-	$(rows[0]).css("text-decoration","underline");
-	
-	};
-};
 
 
 
